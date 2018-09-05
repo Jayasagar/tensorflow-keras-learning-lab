6767892c11765f8d9d9e89c17e06cf49ca243e71
Jayasagar <jayasagar@gmail.com>
2018-09-05 05:18:21 +0200

    ASO-2018 started with basic Neural network impl with TF computation graph

---
+++ BasicNeuralNetwork.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+
+n_input_nodes = 2
+n_output_nodes = 1
+#Inputs
+x = tf.placeholder(tf.float32, (None, n_input_nodes))
+# Weights
+W = tf.Variable(tf.ones((n_input_nodes, n_output_nodes)), dtype=tf.float32)
+# Bias = Threshold
+b = tf.variable(tf.zeros(n_output_nodes), dtype=tf.float32)
+
+z = tf.matmul()
+
