46c697dc785b1ce39d9b05a30e9b91fa41a33b87
Jayasagar <jayasagar@gmail.com>
2018-09-05 04:41:24 +0200

    ASO-2018 the output is still an abstract Tensor -- weve just created a computation graph consisting of operations. To actually get our result, we have to run a session to execute the graph

--- TensorflowBasics.py
+++ TensorflowBasics.py
@@ -2,5 +2,10 @@ import tensorflow as tf
 
 # Creates a node in graph
 a = tf.constant(6, name = 'a')
+b = tf.constant(12, name= 'b')
 
 print('Node a:', a)
+
+c = tf.add(a, b, name= 'c')
+
+print('Node c:', c)
