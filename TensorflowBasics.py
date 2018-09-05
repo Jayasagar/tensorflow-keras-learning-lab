92e031fcd3b2e08339fd1364ee3fac49da360863
Jayasagar <jayasagar@gmail.com>
2018-09-05 04:57:39 +0200

    ASO-2018 first workign TF session computation with basic math operations

--- TensorflowBasics.py
+++ TensorflowBasics.py
@@ -9,3 +9,22 @@ print('Node a:', a)
 c = tf.add(a, b, name= 'c')
 
 print('Node c:', c)
+
+#  Two inout to TF graph flow
+# TensorFlow uses tf.placeholder to handle inputs to the model.
+# tf.placeholder lets you specify that some input will be coming in, of some shape and some type
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+
+z = tf.add(x, y, name= 'z')
+p = tf.subtract(x, 1, name= 'p')
+q = tf.multiply(z, p, name='q')
+
+
+# define a session and run our computation graph:
+
+with tf.Session() as session:
+    feed_dict = {x: 6, y: 6}
+    # pass data in and run the computation graph in a session
+    output = session.run([q], feed_dict = feed_dict)
+    print('TF Session computation output:', output)
