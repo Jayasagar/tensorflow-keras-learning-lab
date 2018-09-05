7dd3543e08cf44d7a156b8c62cd0af68dd2a8336
Jayasagar <jayasagar@gmail.com>
2018-09-05 05:24:08 +0200

    ASO-2018 run multiple operation using 2,3 shape and withotu feed-dict

--- TensorflowBasics.py
+++ TensorflowBasics.py
@@ -21,6 +21,22 @@ p = tf.subtract(x, 1, name= 'p')
 q = tf.multiply(z, p, name='q')
 
 
+# 2-D tensor `a`
+# [[1, 2, 3],
+#  [4, 5, 6]]
+a1 = tf.constant([1, 2, 3, 4, 5, 6], shape=[2, 3], name="a1")
+
+# 2-D tensor `b`
+# [[ 7,  8],
+#  [ 9, 10],
+#  [11, 12]]
+b1 = tf.constant([7, 8, 9, 10, 11, 12], shape=[3, 2], name="b1")
+
+# `a` * `b`
+# [[ 58,  64],
+#  [139, 154]]
+c1 = tf.matmul(a1, b1, name='mul')
+
 # define a session and run our computation graph:
 
 with tf.Session() as session:
@@ -28,3 +44,7 @@ with tf.Session() as session:
     # pass data in and run the computation graph in a session
     output = session.run([q], feed_dict = feed_dict)
     print('TF Session computation output:', output)
+    # Run the Multiple operation directly, without feed_dict as the a1 and b1 has data already.
+    #  Instead I could create placeholders and then define data later :)
+    print ('C:', session.run([c1]))
+
