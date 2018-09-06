7131718adf2dd5c2b1dab88dced31ce26dcf6212
Jayasagar <jayasagar@gmail.com>
2018-09-06 04:54:14 +0200

    ASO-2018 Eurekha :) Now basics Neural Networks works with basic example

--- BasicNeuralNetwork.py
+++ BasicNeuralNetwork.py
@@ -7,8 +7,14 @@ x = tf.placeholder(tf.float32, (None, n_input_nodes))
 # Weights
 W = tf.Variable(tf.ones((n_input_nodes, n_output_nodes)), dtype=tf.float32)
 # Bias = Threshold
-b = tf.variable(tf.zeros(n_output_nodes), dtype=tf.float32)
+b = tf.Variable(tf.zeros(n_output_nodes), dtype=tf.float32)
 
 z = tf.matmul(x, W)
 out = tf.sigmoid(z)
 
+with tf.Session() as session:
+    tf.global_variables_initializer().run(session=session)
+    feed_dict = {x: [[0.25, 0.15]]}
+    output = session.run(out, feed_dict=feed_dict)
+
+    print('output:', output)
