b87699c122acaeb96d9c4c506504741719b64d8d
Jayasagar <jayasagar@gmail.com>
2018-09-09 16:08:28 +0200

    ASO-2019 TF session closes automatically by using with tf.session() as sess:

--- BasicNeuralNetwork.py
+++ BasicNeuralNetwork.py
@@ -17,6 +17,7 @@ b = tf.Variable(tf.zeros(n_output_nodes), dtype=tf.float32)
 z = tf.matmul(x, W)
 out = tf.sigmoid(z)
 
+# close the Session automatically again after printing the output
 with tf.Session() as session:
     # Only after running tf.global_variables_initializer() in a session
     # then only  your variables hold the values we told them to hold
